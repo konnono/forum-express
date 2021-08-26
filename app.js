233d7cac37f22e4b434e41a583445809bfecd21d
konnono <konnono@gmail.com>
2021-08-27 00:56:09 +0800

    Install bcryptjs and body-parser

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 handlebars
+const bodyParser = require('body-parser')
 const db = require('./models')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
-  db.sequelize.sync()
+  // db.sequelize.sync()
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
