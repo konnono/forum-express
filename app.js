1c30bba9c69bc1159763f078439e2d009bd4f973
konnono <konnono@gmail.com>
2021-08-28 18:13:56 +0800

    Install method-override

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const methodOverride = require('method-override')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -17,6 +18,7 @@ app.use(flash())
 // setup passport
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
