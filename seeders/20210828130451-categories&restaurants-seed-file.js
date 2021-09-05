4960cb8be3f4cd36aa0b8080401ebdee8a69060a
konnono <konnono@gmail.com>
2021-09-05 16:20:45 +0800

    Combine categories and restaurants seeders

--- seeders/20210828130451-categories&restaurants-seed-file.js
+++ seeders/20210828130451-categories&restaurants-seed-file.js
@@ -3,6 +3,17 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('categories', ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+      .map((item, index) =>
+      ({
+        id: index * 10 + 1,
+        name: item,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      ), {}
+    )
+
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }).map((d, i) =>
       ({
@@ -16,10 +27,12 @@ module.exports = {
         updatedAt: new Date(),
         CategoryId: Math.floor(Math.random() * 6) * 10 + 1
       })
-      ), {})
+      ), {}
+    )
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Restaurants', null, {})
+    await queryInterface.bulkDelete('Categories', null, {})
   }
 };
