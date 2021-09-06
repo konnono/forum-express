d4fb526d9182297aa64049f7dbd0ad9971ec1ae5
konnono <konnono@gmail.com>
2021-09-06 22:24:26 +0800

    Add addFollowing and removeFollowing to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const Comment = db.Comment
 const Restaurant = db.Restaurant
 const Favorite = db.Favorite
 const Like = db.Like
+const Followship = db.Followship
 const helpers = require('../_helpers')
 const fs = require('fs')
 const imgur = require('imgur-node-api')
@@ -202,6 +203,31 @@ let userController = {
       users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
       return res.render('topUser', { users })
     })
+  },
+
+  addFollowing: (req, res) => {
+    return Followship.create({
+      followerId: helpers.getUser(req).id,
+      followingId: req.params.userId
+    })
+      .then(followship => {
+        return res.redirect('back')
+      })
+  },
+
+  removeFollowing: (req, res) => {
+    return Followship.findOne({
+      where: {
+        followerId: helpers.getUser(req).id,
+        followingId: req.params.userId
+      }
+    })
+      .then(followship => {
+        followship.destroy()
+          .then((followship) => {
+            return res.redirect('back')
+          })
+      })
   }
 }
 
