dfe510cb6360e786e25b223beca9cc85bc308f58
konnono <konnono@gmail.com>
2021-09-06 21:44:18 +0800

    Revert "Merge branch 'A21'"
    This reverts commit c3e5d8b62e64b0c48f945819911ae82a67852267, reversing
    changes made to f7b7faeee9d57f492cb323e6fa2922bad6adf0b7.

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,6 @@ const User = db.User
 const Comment = db.Comment
 const Restaurant = db.Restaurant
 const Favorite = db.Favorite
-const Like = db.Like
 const helpers = require('../_helpers')
 const fs = require('fs')
 const imgur = require('imgur-node-api')
@@ -138,7 +137,7 @@ let userController = {
       UserId: helpers.getUser(req).id,
       RestaurantId: req.params.restaurantId
     })
-      .then(restaurant => {
+      .then((restaurant) => {
         return res.redirect('back')
       })
   },
@@ -150,34 +149,9 @@ let userController = {
         RestaurantId: req.params.restaurantId
       }
     })
-      .then(favorite => {
+      .then((favorite) => {
         favorite.destroy()
-          .then(restaurant => {
-            return res.redirect('back')
-          })
-      })
-  },
-
-  addLike: (req, res) => {
-    return Like.create({
-      UserId: helpers.getUser(req).id,
-      RestaurantId: req.params.restaurantId
-    })
-      .then(restaurant => {
-        return res.redirect('back')
-      })
-  },
-
-  removeLike: (req, res) => {
-    return Like.findOne({
-      where: {
-        UserId: helpers.getUser(req).id,
-        RestaurantId: req.params.restaurantId
-      }
-    })
-      .then(like => {
-        like.destroy()
-          .then(restaurant => {
+          .then((restaurant) => {
             return res.redirect('back')
           })
       })
